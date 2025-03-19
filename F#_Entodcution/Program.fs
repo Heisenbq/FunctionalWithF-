5261a43a112ff10752625035297f7f4a74a5d209
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2025-03-19 16:32:51 +0300

    1.1 and 1.2 tasks

---
+++ F#_Entodcution/Program.fs
@@ -0,0 +1,41 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+
+let solve (a,b,c) = 
+    let D = b*b - 4.0 *a*c in
+        ((-b + sqrt(D))/ (2.0 * a) , (-b - sqrt(D))/ (2.0 * a));;
+solve(1.0, 2.0, -3.0);;
+
+
+
+
+//let solve (a,b,c) = 
+//    let D = b*b - 4.0 *a*c in
+//        ((-b + sqrt(D))/ (2.0 * a) , (-b - sqrt(D))/ (2.0 * a))
+//in solve(1.0 , 2.0, -3.0)
+
+//(-2.0 + sqrt(2.0*2.0 - 4.0*(-3.0))) / 2.0 ;;
+
+//let a =1.0 in
+//    let b = 2.0 in
+//            let c = -3.0 in
+//                let D = b*b - 4.0 * a * c in
+//                    ((-b + sqrt(D))/ (2.0 * a) , (-b - sqrt(D))/ (2.0 * a)) ;;
+
+//let a =1.0 in
+//    let b = 2.0 in
+//            let c = -3.0 in
+//                let D = b*b - 4.0 * a * c in
+//                    [(-b + sqrt(D))/ (2.0 * a) , (-b - sqrt(D))/ (2.0 * a)] ;;
+
+//let solve (a,b,c) = 
+//    let D = b*b - 4.0 *a*c in
+//        ((-b + sqrt(D))/ (2.0 * a) , (-b - sqrt(D))/ (2.0 * a));;
+//solve(1.0, 2.0, -3.0);;
+
+
+//let solve (a,b,c) = fun(a,b,c) -> 
+//    let D = b*b - 4.0 *a*c
+//    ((-b + sqrt(D))/ (2.0 * a) , (-b - sqrt(D))/ (2.0 * a))
+//in solve(1.0 , 2.0, -3.0);;
