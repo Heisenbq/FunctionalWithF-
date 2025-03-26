9b4c9c7f99b12780a728b4ce4adcb4154f9dba31
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2025-03-26 14:52:51 +0300

    complete task 6 and 7

--- F#_Entodcution/Program.fs
+++ F#_Entodcution/Program.fs
@@ -22,34 +22,80 @@ let cylinderVolumeCar height radius = height * circleArea radius
 
 let rec sumOfDigits n =
     if n = 0 then 0  
-    else (n % 10) + sumOfDigits (n / 10)
+    else (n % 10) + sumOfDigits (n / 10);;
+
+let sumOfDig n = 
+    let rec sumOfDigitsTail n acc =
+       if n = 0 then acc
+       else sumOfDigitsTail (n/10) (acc + (n%10))
+    sumOfDigitsTail n 0 ;;
+
+let colOfDiv n =
+    let rec colOfDivTail n acc i =       
+        match i with
+        | 0 -> acc
+        | _ -> colOfDivTail n (acc + (if n % i = 0 then 1 else 0)) (i - 1)
+    colOfDivTail n 0 n ;;
 
 
-let rec sumOfDigitsTail n acc =
-    if n = 0 then acc
-    else sumOfDigitsTail (n/10) (acc + (n%10))
+let rec Fibonacci n = 
+    match n with
+        |0 -> 0
+        |1 -> 1
+        |_ -> Fibonacci(n-1) + Fibonacci(n-2) ;;
+
+let fib n =
+    let rec FibonacciTail n a b =
+        match n with
+        | 0 -> a
+        | 1 -> b
+        | _ -> FibonacciTail (n - 1) b (a + b)
+    FibonacciTail n 0 1 ;;
+ 
+ let chooseFunction isSum =
+    if isSum then
+        colOfDiv  // Возвращаем функцию для подсчета суммы цифр
+    else
+        fib ;; 
 
 
+let multiplyOp acc digit = acc * digit;;
 
-[<EntryPoint>]
-let main argv =
+let funcOfDigits n func initValue= 
+    let rec funcOfDigitsTail n acc =
+       match n with
+       | 0 -> acc
+       | _ -> funcOfDigitsTail (n/10) (func acc (n%10))
+    funcOfDigitsTail n initValue;;
+
+funcOfDigits 237 multiplyOp 1;;
     
-    let res = sumOfDigitsTail 127 0
-    printfn "%i" res
-    //printf "Введите радиус круга: "
-    //let radius = Double.Parse(Console.ReadLine())
 
-    //printf "Введите высоту цилиндра: "
-    //let height = Double.Parse(Console.ReadLine())
+ //let fib_Or_SumDigits n =
+ //   match n with
+ //       |true -> fib
+ //       |false ->sumOfDig
+       
 
-    ////let volume = cylinderVolume height radius  
-    ////printfn "Объем цилиндра: %f" volume
+//[<EntryPoint>]
+//let main argv =
+    
+//    let res = fib 19
+//    printfn "%i" res
+//    //printf "Введите радиус круга: "
+//    //let radius = Double.Parse(Console.ReadLine())
 
-    //let volumeCar = cylinderVolume height  
-    //let volume = volumeCar radius
-    //printfn "Объем цилиндра: %f" volume
+//    //printf "Введите высоту цилиндра: "
+//    //let height = Double.Parse(Console.ReadLine())
 
-    0  
+//    ////let volume = cylinderVolume height radius  
+//    ////printfn "Объем цилиндра: %f" volume
+
+//    //let volumeCar = cylinderVolume height  
+//    //let volume = volumeCar radius
+//    //printfn "Объем цилиндра: %f" volume
+
+//    0  
 
 
 
--- F#_Entodcution/obj/Debug/net8.0/F#_Entodcution.AssemblyInfo.fs
+++ F#_Entodcution/obj/Debug/net8.0/F#_Entodcution.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("F#_Entodcution")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044b119aaebc26ceac79aeb42ced470da835de4d")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a2ff0a06bc99800f5b4114cf5385a1d38a1501a")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("F#_Entodcution")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("F#_Entodcution")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
