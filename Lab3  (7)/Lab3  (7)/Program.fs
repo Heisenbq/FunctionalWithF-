683a9c4deb7031ead6db6a83744962e8e6541ef2
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2025-04-09 16:51:42 +0300

    7.1 part 2 GeometricShape algeibraic

--- Lab3  (7)/Lab3  (7)/Program.fs
+++ Lab3  (7)/Lab3  (7)/Program.fs
@@ -1,22 +1,30 @@
 ﻿
 
 open System
-open GeometricShape
+//open GeometricShape
 open IPrint
+
+
+type GeometricShape =
+    | Rectangle of width: float * height: float  
+    | Square of side: float                     
+    | Circle of radius: float                   
+
+let getArea shape =
+    match shape with
+    | Rectangle (width, height) -> width * height  
+    | Square side -> side * side                  
+    | Circle radius -> Math.PI * radius * radius  
+
 [<EntryPoint>]
 let main argv =
 
-    let rectangle = Rectangle(5.0, 3.0)
+    let rectangle = Rectangle (5.0, 3.0)
+    let square = Square 4.0
+    let circle = Circle 3.0
 
-
-    let square = Square(4.0)
-
-
-    let circle = Circle(3.0)
-
-
-    (rectangle :> IPrint).Print()
-    (square :> IPrint).Print()
-    (circle :> IPrint).Print()  
+    printfn "Area of Rectangle: %.2f" (getArea rectangle)
+    printfn "Area of Square: %.2f" (getArea square)
+    printfn "Area of Circle: %.2f" (getArea circle)
 
     0 
