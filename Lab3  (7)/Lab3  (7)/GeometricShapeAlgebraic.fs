582ade1058659b980c8e7257708353944f474125
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2025-04-09 17:01:02 +0300

    7.1 part 2 refactoring

---
+++ Lab3  (7)/Lab3  (7)/GeometricShapeAlgebraic.fs
@@ -0,0 +1,14 @@
+﻿module GeometricShapeAlgebraic
+open System
+
+type GeometricShapeAlg =
+    | RectangleA of width: float * height: float  
+    | SquareA of side: float                     
+    | CircleA of radius: float                   
+
+let getArea shape =
+    match shape with
+    | RectangleA (width, height) -> width * height  
+    | SquareA side -> side * side                  
+    | CircleA radius -> Math.PI * radius * radius  
+
--- Lab3  (7)/Lab3  (7)/Program.fs
+++ Lab3  (7)/Lab3  (7)/Program.fs
@@ -1,30 +1,44 @@
 ﻿
 
 open System
-//open GeometricShape
+open GeometricShape
+open GeometricShapeAlgebraic
 open IPrint
 
 
-type GeometricShape =
-    | Rectangle of width: float * height: float  
-    | Square of side: float                     
-    | Circle of radius: float                   
+let checkGeometricShape() =
+    let rectangle = Rectangle(5.0, 3.0)
 
-let getArea shape =
-    match shape with
-    | Rectangle (width, height) -> width * height  
-    | Square side -> side * side                  
-    | Circle radius -> Math.PI * radius * radius  
+
+    let square = Square(4.0)
+
+
+    let circle = Circle(3.0)
+
+
+    (rectangle :> IPrint).Print()
+    (square :> IPrint).Print()
+    (circle :> IPrint).Print() 
+
+
+let checkGeometricShapeAlgebraic() =
+    let rectangle = RectangleA (5.0, 3.0)
+    let square = SquareA 4.0
+    let circle = CircleA 3.0
+
+    
+    printfn "Area of Rectangle: %.2f" (getArea rectangle)
+    printfn "Area of Square: %.2f" (getArea square)
+    printfn "Area of Circle: %.2f" (getArea circle)
+
 
 [<EntryPoint>]
 let main argv =
 
-    let rectangle = Rectangle (5.0, 3.0)
-    let square = Square 4.0
-    let circle = Circle 3.0
+    
 
-    printfn "Area of Rectangle: %.2f" (getArea rectangle)
-    printfn "Area of Square: %.2f" (getArea square)
-    printfn "Area of Circle: %.2f" (getArea circle)
+
+    //checkGeometricShape()
+    //checkGeometricShapeAlgebraic()
 
     0 
